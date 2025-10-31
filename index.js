940bec8b255c5e4137a053d66b8ded00aca3e5ba
Damian Martinez <DCAM.DAMIAN.DM@GMAIL.COM>
2025-10-30 21:03:44 -0600

    Block12 Secure the vault

---
+++ index.js
@@ -0,0 +1,17 @@
+let code1 = 7+3;
+console.log(code1);
+
+let code2 = 160/2-40;
+console.log(code2);
+
+let code3 = 35+24%5;
+console.log(code3);
+
+let message = "The vault has been secured.  The combination is:";
+console.log(message);
+
+let codeA = String(code1) + "-" + String(code2) + "-" + String(code3);
+console.log(codeA);
+
+let codeB = `${code1}-${code2}-${code3}`;
+console.log(codeB);
